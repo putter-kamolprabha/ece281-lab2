ba3ffeb98ea30db73ba3b486782da4ccb70ed458
putter-kamolprabha <c27thinnaphat.kamolprabha@afacademy.af.edu>
2025-03-04 23:24:47 -0700

    Update_3

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -52,37 +52,37 @@ begin
     test_process : process 
     begin
          w_sw <= x"0"; wait for 10 ns;
-         assert w_seg = "1000000" report "Error on 0" SEVERITY FAILURE; --flipped
+         assert NOT w_seg = "0111111" report "Error on 0" SEVERITY FAILURE; --flipped
          w_sw <= x"1"; wait for 10 ns;
-         assert w_seg = "1111001" report "Error on 1" SEVERITY FAILURE;
+         assert NOT w_seg = "0000110" report "Error on 1" SEVERITY FAILURE;
          w_sw <= x"2"; wait for 10 ns;
-         assert w_seg = "0100100" report "Error on 2" SEVERITY FAILURE;
+         assert NOT w_seg = "1011011" report "Error on 2" SEVERITY FAILURE;
          w_sw <= x"3"; wait for 10 ns;
-         assert w_seg = "011000" report "Error on 3" SEVERITY FAILURE;
+         assert NOT w_seg = "1001111" report "Error on 3" SEVERITY FAILURE;
          w_sw <= x"4"; wait for 10 ns;
-         assert w_seg = "0011001" report "Error on 4" SEVERITY FAILURE;
+         assert NOT w_seg = "1100110" report "Error on 4" SEVERITY FAILURE;
          w_sw <= x"5"; wait for 10 ns;
-         assert w_seg = "0010010" report "Error on 5" SEVERITY FAILURE;
+         assert NOT w_seg = "1101101" report "Error on 5" SEVERITY FAILURE;
          w_sw <= x"6"; wait for 10 ns;
-         assert w_seg = "0000010" report "Error on 6" SEVERITY FAILURE;
+         assert NOT w_seg = "1111101" report "Error on 6" SEVERITY FAILURE;
          w_sw <= x"7"; wait for 10 ns;
-         assert w_seg = "1111000" report "Error on 7" SEVERITY FAILURE;
+         assert NOT w_seg = "0000111" report "Error on 7" SEVERITY FAILURE;
          w_sw <= x"8"; wait for 10 ns;
-         assert w_seg = "0000000" report "Error on 8" SEVERITY FAILURE;
+         assert NOT w_seg = "1111111" report "Error on 8" SEVERITY FAILURE;
          w_sw <= x"9"; wait for 10 ns;
-         assert w_seg = "0010000" report "Error on 9" SEVERITY FAILURE;
+         assert NOT w_seg = "1101111" report "Error on 9" SEVERITY FAILURE;
          w_sw <= x"A"; wait for 10 ns;
-         assert w_seg = "0001000" report "Error on A" SEVERITY FAILURE;
+         assert NOT w_seg = "1110111" report "Error on A" SEVERITY FAILURE;
          w_sw <= x"B"; wait for 10 ns;
-         assert w_seg = "0000011" report "Error on B" SEVERITY FAILURE;
+         assert NOT w_seg = "1111100" report "Error on B" SEVERITY FAILURE;
          w_sw <= x"C"; wait for 10 ns;
-         assert w_seg = "0100111" report "Error on C" SEVERITY FAILURE;
+         assert NOT w_seg = "1011000" report "Error on C" SEVERITY FAILURE;
          w_sw <= x"D"; wait for 10 ns;
-         assert w_seg = "0100001" report "Error on D" SEVERITY FAILURE;
+         assert NOT w_seg = "1011110" report "Error on D" SEVERITY FAILURE;
          w_sw <= x"E"; wait for 10 ns;
-         assert w_seg = "0000110" report "Error on E" SEVERITY FAILURE;
+         assert NOT w_seg = "1111001" report "Error on E" SEVERITY FAILURE;
          w_sw <= x"F"; wait for 10 ns;
-         assert w_seg = "0001110" report "Error on F" SEVERITY FAILURE;
+         assert NOT w_seg = "1110001" report "Error on F" SEVERITY FAILURE;
         
          wait; 
     end process test_process;
