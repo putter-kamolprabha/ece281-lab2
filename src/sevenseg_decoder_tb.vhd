470aee83b186353c165e591b62a8b4089c8c8207
putter-kamolprabha <c27thinnaphat.kamolprabha@afacademy.af.edu>
2025-03-05 17:37:17 -0700

    Update_3

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -18,74 +18,57 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+ 
 entity sevenseg_decoder_tb is
 end sevenseg_decoder_tb;
-
-architecture Behavioral of sevenseg_decoder_tb is
-    component sevenseg_decoder is 
-        Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
-    end component; 
-    
-    signal w_sw : STD_LOGIC_VECTOR(3 DOWNTO 0) := (others => '0');
-    signal w_seg : STD_LOGIC_VECTOR(6 DOWNTO 0);
-
-begin
-    sevenseg_decoder_inst : sevenseg_decoder PORT MAP (
-        i_Hex => w_sw,
-        o_seg_n => w_seg
+ 
+architecture test_bench of sevenseg_decoder_tb is
+ 
+--Declare the component under test (UUT): sevenseg_decoder
+ 
+  component sevenseg_decoder is
+    port(
+      i_Hex   : in  std_logic_vector(3 downto 0);
+      o_seg_n : out std_logic_vector(6 downto 0)
     );
-    
-    test_process : process 
-    begin
-         w_sw <= x"0"; wait for 10 ns;
-         assert NOT w_seg = "0111111" report "Error on 0" SEVERITY FAILURE; --flipped
-         w_sw <= x"1"; wait for 10 ns;
-         assert NOT w_seg = "0000110" report "Error on 1" SEVERITY FAILURE;
-         w_sw <= x"2"; wait for 10 ns;
-         assert NOT w_seg = "1011011" report "Error on 2" SEVERITY FAILURE;
-         w_sw <= x"3"; wait for 10 ns;
-         assert NOT w_seg = "1001111" report "Error on 3" SEVERITY FAILURE;
-         w_sw <= x"4"; wait for 10 ns;
-         assert NOT w_seg = "1100110" report "Error on 4" SEVERITY FAILURE;
-         w_sw <= x"5"; wait for 10 ns;
-         assert NOT w_seg = "1101101" report "Error on 5" SEVERITY FAILURE;
-         w_sw <= x"6"; wait for 10 ns;
-         assert NOT w_seg = "1111101" report "Error on 6" SEVERITY FAILURE;
-         w_sw <= x"7"; wait for 10 ns;
-         assert NOT w_seg = "0000111" report "Error on 7" SEVERITY FAILURE;
-         w_sw <= x"8"; wait for 10 ns;
-         assert NOT w_seg = "1111111" report "Error on 8" SEVERITY FAILURE;
-         w_sw <= x"9"; wait for 10 ns;
-         assert NOT w_seg = "1101111" report "Error on 9" SEVERITY FAILURE;
-         w_sw <= x"A"; wait for 10 ns;
-         assert NOT w_seg = "1110111" report "Error on A" SEVERITY FAILURE;
-         w_sw <= x"B"; wait for 10 ns;
-         assert NOT w_seg = "1111100" report "Error on B" SEVERITY FAILURE;
-         w_sw <= x"C"; wait for 10 ns;
-         assert NOT w_seg = "1011000" report "Error on C" SEVERITY FAILURE;
-         w_sw <= x"D"; wait for 10 ns;
-         assert NOT w_seg = "1011110" report "Error on D" SEVERITY FAILURE;
-         w_sw <= x"E"; wait for 10 ns;
-         assert NOT w_seg = "1111001" report "Error on E" SEVERITY FAILURE;
-         w_sw <= x"F"; wait for 10 ns;
-         assert NOT w_seg = "1110001" report "Error on F" SEVERITY FAILURE;
-        
-         wait; 
-    end process test_process;
-
-
-end Behavioral;
+  end component;
+ 
+  -- declare any additional components required
+ 
+  signal w_Hex   : std_logic_vector(3 downto 0) := (others => '0');
+  signal w_seg_n : std_logic_vector(6 downto 0);
+ 
+begin
+  my_decoder_inst : sevenseg_decoder port map(
+      i_Hex   => w_Hex,
+      o_seg_n => w_seg_n
+    );
+ 
+ 
+  --Test Process: Check 4 different hex values and assert expected outputs
+  -----------------------------------------------------------------------------
+  test_process : process
+  begin
+    -- 0
+    w_Hex <= x"0";  wait for 10 ns;
+    assert (w_seg_n = "1000000") report "Error on 0" severity failure;
+ 
+    -- 1
+    w_Hex <= x"1";  wait for 10 ns;
+    assert (w_seg_n = "1111001") report "Error on 1" severity failure;
+ 
+    -- E
+    w_Hex <= x"E";  wait for 10 ns;
+    assert (w_seg_n = "0000110") report "Error on E" severity failure;
+ 
+    -- F
+    w_Hex <= x"F";  wait for 10 ns;
+    assert (w_seg_n = "0001110") report "Error on F" severity failure;
+ 
+    wait;
+  end process;
+ 
+end test_bench;
